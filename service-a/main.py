dc4a60be374120c8c2290e064ec75642ffa5664e
blessing-bester <blessingphir79@gmail.com>
2025-10-19 16:22:06 +0200

    Update Service A

--- service-a/main.py
+++ service-a/main.py
@@ -9,3 +9,31 @@ app = FastAPI(title="Service A - Data Provider")
 REQUEST_COUNT = Counter("service_a_total_requests", "Total requests to Service A", ["endpoint"])
 LATENCY = Histogram("service_a_latency_seconds", "Latency  for each request", ["endpoint"])
 
+@app.get("/")
+def root():
+    return {"message:" "Service A is now live!"}
+
+@app.get("/data")
+def get_data():
+    REQUEST_COUNT.labels(endpoint="/data").inc()
+    start = time.time()
+
+    # variable latency (100ms-2s)
+    delay = random.uniform(0.1, 2.0)
+    time.sleep(delay)
+
+    LATENCY.labels(endpoint="/data").observe(time.time() - start)
+
+    sample_data = {
+        "id": random.randint(1, 100)
+        "value": random.choice(["one", "two", "three", "four"])
+        "delay": round(delay, 3)
+    }
+    return sample_data
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
+
+
+    
